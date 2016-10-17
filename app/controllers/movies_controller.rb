7ced277576f5b5b8eb867022e332c77a2b3d82c3
Kyle Christiansen <kylec2013@gmail.com>
2016-10-17 21:42:00 +0000

    holy cow i've been forgetting to commit... massive dump of code here pls

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -61,14 +61,29 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
   
-  def search_tmdb
-    @movies=Movie.find_in_tmdb(params[:search_terms])
-   if @search_terms.blank?
-      flash[:alert] = "Invalid search terms"
-      redirect_to movies_path and return
+    def search_tmdb
+    @search_terms = params[:search_terms]
+    if @search_terms.to_s.strip.length == 0
+      flash[:notice] = "Invalid search term"
+      redirect_to movies_path
+    else
+      @movies=Movie.find_in_tmdb(@search_terms)
+      if @movies == []
+        flash[:notice] = "No matching movies were found on TMDb"
+        redirect_to movies_path
+      end
     end
-    @movies=Movie.find_in_tmdb(params[:search_terms])
   end
-
-
+  
+  def add_tmdb
+    if params[:tmdb_movies]==nil
+      flash[:notice] = "No movies selected"
+    else
+      params[:tmdb_movies].keys.each do |key|
+        Movie.create_from_tmdb(key)
+      end
+      flash[:notice] = "Movies successfully added to Rotten Potatoes"
+    end
+    redirect_to movies_path
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,46 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
-    %w(G PG PG-13 NC-17 R)
+   %w(G PG PG-13 NC-17 R NA)
   end
   
-# class Movie::InvalidKeyError < StandardError ; end
+def self.find_in_tmdb(string)
+    movie_list_hash = []
+    Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+    movies_raw = []
+    begin
+      movies_raw = Tmdb::Movie.find(string)
+      
+    rescue Tmdb::InvalidApiKeyError
+        raise Movie::InvalidKeyError, 'Invalid API key'
+    end
+    movies_raw.each do |movie|
+      releases = Tmdb::Movie.releases(movie.id)["countries"]
+      rating = Movie.getRating(releases)
+      
+      if(rating != nil) then
+        movie_list_hash.push({:tmdb_id => movie.id, :title => movie.title, :rating => rating, :release_date => movie.release_date})
+      end
+    end
+    
+    return movie_list_hash
+  end
   
-#  def self.find_in_tmdb(string)
-#    begin
-#      Tmdb::Movie.find(string)
-#    rescue Tmdb::InvalidApiKeyError
-#        raise Movie::InvalidKeyError, 'Invalid API key'
-#    end
-#  end
-
+  def self.getRating(releases)
+    rating = nil
+    releases.each do |release|
+      if (release["iso_3166_1"] == "US" && (release["certification"] == "G" || release["certification"] == "PG" || release["certification"] == "PG-13" || release["certification"] == "R" || release["certification"] == "NC-17")) then
+        rating = release["certification"]
+      end
+    end
+    return rating
+  end
+  
+  def self.create_from_tmdb(tmdb_movie_id)
+    Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+    movie_info = Tmdb::Movie.detail(tmdb_movie_id)
+    releases = Tmdb::Movie.releases(tmdb_movie_id)["countries"]
+    rating = Movie.getRating(releases)
+    newMovie = Movie.new(:title => movie_info["original_title"], :release_date => movie_info["release_date"], :rating => rating)
+    newMovie.save
+  end
 end
